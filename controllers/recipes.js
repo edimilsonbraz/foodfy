426f6f719a43019dc996c85105211ed59ee0f75c
edimilsonbraz <edimilson.gt8@gmail.com>
2020-07-24 08:19:54 -0300

    add page and method edit, delete

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -2,6 +2,16 @@ const fs = require('fs')
 const data = require("../data.json")
 
 
+exports.index = function(req, res) {   
+
+    return res.render("admin/recipes/index")
+}
+
+exports.create = function(req, res) {  
+
+    return res.render("admin/recipes/create")
+}
+
 exports.post = function(req, res) {
 
         const keys = Object.keys(req.body)
@@ -45,7 +55,7 @@ exports.show = function(req, res) {
     const { id } = req.params
     
     const foundRecipe = data.recipes.find(function(recipe) {
-        return recipe.id == id
+        return id == recipe.id
     })
 
     if(!foundRecipe) return res.send("Recipe not found")
@@ -55,5 +65,58 @@ exports.show = function(req, res) {
 
 exports.edit = function(req, res) {
 
-    return res.send("Está funfando")
+    const { id } = req.params
+    
+    const foundRecipe = data.recipes.find(function(recipe) {
+        return recipe.id == id
+    })
+
+    if(!foundRecipe) return res.send("Recipe not found")
+
+    return res.render("admin/recipes/edit", { recipe: foundRecipe })
+}
+
+exports.put = function(req, res) {
+    
+    const { id } = req.body
+    let index = 0
+
+    const foundRecipe = data.recipes.find(function(recipe, foundIndex) {
+        if (id == recipe.id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if(!foundRecipe) return res.send("Recipe not found")
+
+    const recipe = {
+        ...foundRecipe,
+        ...req.body
+    }
+
+    data.recipes[index] = recipe
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("Write file error!")
+
+        return res.redirect(`/admin/recipes/${id}`)
+    })
+    
+
+}
+
+exports.delete = function(req, res) {
+    const { id } = req.body
+
+    const filteredRecipes = data.recipes.filter(function(recipe) {
+        return recipe.id != id
+    })
+
+    data.recipes = filteredRecipes
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("write file error!")
+
+        return res.redirect("/admin/recipes")
+    })
 }
--- routes.js
+++ routes.js
@@ -3,29 +3,17 @@ const routes = express.Router()
 const recipes = require('./controllers/recipes')
 
 
-
 routes.get('/', function(req, res) {
     return res.redirect("/admin/recipes")
 })
 
-
-
-routes.get("/admin/recipes", function(req, res) {   // Mostrar a Lista de receitas
-
-    return res.render("admin/recipes/index")
-})
-     
-routes.get("/admin/recipes/create", function(req, res) {  // Mostrar formulário de nova receita
-
-    return res.render("admin/recipes/create")
-}) 
+routes.get("/admin/recipes", recipes.index)  // Mostrar a Lista de receitas
+routes.get("/admin/recipes/create", recipes.create) // Mostrar formulário de nova receita
 routes.get("/admin/recipes/:id", recipes.show) // Exibir detalhes de uma receita
-
 routes.get("/admin/recipes/:id/edit", recipes.edit) // Mostrar formulário de edição de receita
-
-routes.post("/admin/recipes", recipes.post) 
-// routes.get("/admin/recipes", recipes.put) // Editar uma receita
-// routes.get("/admin/recipes", recipes.delete)  // Deletar uma receita
+routes.post("/admin/recipes", recipes.post) // Criar uma nova receita
+routes.put("/admin/recipes", recipes.put) // Atualizar uma receita
+routes.delete("/admin/recipes", recipes.delete)  // Deletar uma receita
 
 
 module.exports = routes
