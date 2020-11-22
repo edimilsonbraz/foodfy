d5d23e38dde3f6c043ecab239d1b6cb1bc1a4451
edimilsonbraz <edimilson.gt8@gmail.com>
2020-11-22 16:27:55 -0300

    update menuAdmin

--- public/scripts/ActiveMenuAdmin.js
+++ public/scripts/ActiveMenuAdmin.js
@@ -1,12 +1,10 @@
 
 // CÓDIGO PARA MANTER LINK DO MENU SELECIONADO
-    const currentPage = location.pathname
-    const menuLinks = document.querySelectorAll( 'header nav .nav__links li a')
+    const currentPageAdmin = location.pathname
+    const menuLinks = document.querySelectorAll( 'section .links a')
     for ( link of menuLinks ) {
         if ( currentPage.includes ( link.getAttribute ('href') )) {
             link.classList.add ( 'active' )
         }
-}
-
-    
+    }
         
